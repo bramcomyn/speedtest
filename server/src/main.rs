87ab951f15e2db30907f08cdc9782669fd78fd45
Bram Comyn <bram.comyn@ugent.be>
2025-09-05 20:02:04 +0200

    feat: server multithreading

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,8 @@
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
-async fn handle(mut socket: TcpStream) -> tokio::io::Result<()> {
+fn handle(mut socket: TcpStream) -> std::io::Result<()> {
     let mut buf = [0u8; 64 * 1024];
     socket.read_exact(&mut buf[0..1])?;
 
@@ -8,41 +10,42 @@ async fn handle(mut socket: TcpStream) -> tokio::io::Result<()> {
         match buf[0] {
             0 => {
                 let n = socket.read(&mut buf[1..])?;
-                if n == 0 { break ; }
-                socket.write_all(&buf[1..n+1])?;
+                if n == 0 {
+                    break;
+                }
+                socket.write_all(&buf[1..n + 1])?;
                 socket.flush()?;
-            },
+            }
             1 => {
                 loop {
                     if let Err(_e) = socket.write_all(&buf) {
                         return Ok(());
                     }
                 }
-            },
+            }
             2 => {
                 loop {
                     if let Err(_e) = socket.read_exact(&mut buf) {
                         return Ok(());
                     }
                 }
-            },
+            }
             _ => break,
         }
     }
-    
+
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> tokio::io::Result<()> {
+fn main() -> std::io::Result<()> {
     let addr = "0.0.0.0:5001";
     println!("Echo server listening on {addr}");
     let listener = TcpListener::bind(addr)?;
     loop {
         let (sock, peer) = listener.accept()?;
         println!("Accepted connection from {peer}");
-        tokio::spawn(async move {
-            if let Err(e) = handle(sock).await {
+        thread::spawn(move || {
+            if let Err(e) = handle(sock) {
                 eprintln!("connection error: {e}");
             }
         });
